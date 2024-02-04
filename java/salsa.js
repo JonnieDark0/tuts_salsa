227181bf03ae0df035f90f25f3ea12d91dc7795c
JonnieDark0 <158841928+JonnieDark0@users.noreply.github.com>
2024-02-04 12:25:33 -0600

    Add files via upload

---
+++ java/salsa.js
@@ -0,0 +1,24 @@
+
+/*
+let myVideo1 = document.getElementById("video1"); 
+
+function playPause1() { 
+  if (myVideo1.paused) 
+    myVideo1.play(); 
+  else 
+    myVideo1.pause(); 
+} 
+
+function makeBig1() { 
+    myVideo1.width = 720; 
+} 
+
+function makeSmall() { 
+    myVideo1.width = 320; 
+} 
+
+function makeNormal1() { 
+    myVideo1.width = 420; 
+} 
+*/
+
